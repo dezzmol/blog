2e313ffb2c384f07f869e4cc6530ede3d06b908f
Radmir <musabaevradmir@gmail.com>
2023-08-15 23:24:32 +0400

    Added time and date for comments

--- src/modules/PostID/components/Comments.tsx
+++ src/modules/PostID/components/Comments.tsx
@@ -8,15 +8,23 @@ interface CommentsProps {
 }
 
 const Comments: FC<CommentsProps> = ({comments}) => {
+
+    const toDateTime = (secs: number) => {
+        let t = new Date(Date.UTC(1970, 0, 1)); // Epoch
+        t.setUTCSeconds(secs);
+        return t.toLocaleString('en-GB',{timeZone:'UTC'});
+    }
+
     return (
         <Paper style={{padding: '16px'}}>
             <CommentForm/>
             <h2>Comments:</h2>
             {comments.map(comment =>
-                <div>
+                <Paper style={{padding: '10px', marginTop: '10px'}}>
                     <h4>{comment.userDisplayName}</h4>
+                    <h6>{toDateTime(comment.date.seconds)}</h6>
                     {comment.body}
-                </div>
+                </Paper>
 
             )}
         </Paper>
